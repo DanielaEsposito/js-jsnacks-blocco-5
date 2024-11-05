2ca677f12c9d8db9ddd420303411bf2b722630e1
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-05 15:01:12 +0100

    add snack 1

--- snack-1/script.js
+++ snack-1/script.js
@@ -1,5 +1,5 @@
 const names = ['Edoardo', 'Simone', 'Francesco'];
-console.log(names);
+console.log(names.join(","));
 
 // Stampa in console tutti i nomi
 // Risultato: 'Edoardo', 'Simone', 'Francesco'
