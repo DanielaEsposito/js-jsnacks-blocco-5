79471964d8f3e2e09b740a5ab409b7f7e79159d5
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-05 14:49:35 +0100

    add snack 7

--- snack-7/script.js
+++ snack-7/script.js
@@ -18,6 +18,15 @@ const students = [
     class: '5B'
   },
 ];
+let searchedStudent = {}
+for(let i = 0; i < students.length ; i++){
+  const student = students[i];
+  if(student.id === 2){
+  searchedStudent = student
+  }
+}
+console.log(searchedStudent);
+
 
 // Recupera i dati dello studente con id 2
 // Risultato: { id: 2, name: 'Mario Banfi', age: 34, class: '4A' }
