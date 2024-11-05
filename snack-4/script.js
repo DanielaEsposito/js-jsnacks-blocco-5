9289f46376dfeca9a375fd3dd50ee8589922cc16
DanielaEsposito <esposito.daniela9817@gmail.com>
2024-11-05 14:29:24 +0100

    complite snacks 4 6

--- snack-4/script.js
+++ snack-4/script.js
@@ -4,6 +4,13 @@ const posts = [
   { author: 'Fabrizio', date: '12/9/2020', content: 'lorem ipsum...' },
   { author: 'Enrico', date: '09/12/2021', content: 'lorem ipsum...' },
 ]
+const postsNmae = [];
+posts.forEach((posts)=>{
+  postsNmae.push(posts.author);
+})
+console.log(postsNmae);
+
+
 
 // Crea un nuovo array coi nomi di tutti gli autori
 // Risultato: ['Marco', 'Luca', 'Fabrizio', 'Enrico']
